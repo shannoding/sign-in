0d0ed476dcd34bb0602dbc453b68791858dfec16
Shannon Ding <shannon_ding@yahoo.com>
2017-07-06 10:30:41 -0700

    everything

---
+++ signin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  signin
+//
+//  Created by Shannon Ding on 7/6/17.
+//  Copyright © 2017 Shannon Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
