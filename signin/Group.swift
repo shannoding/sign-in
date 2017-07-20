f608f5415f5a81639e8731700623df9a605b45e1
Shannon Ding <shannon_ding@yahoo.com>
2017-07-19 17:03:22 -0700

    save group to database

--- signin/Group.swift
+++ signin/Group.swift
@@ -5,24 +5,21 @@
 //  Created by Shannon Ding on 7/19/17.
 //  Copyright © 2017 Shannon Ding. All rights reserved.
 //
-/*
+
 import Foundation
 import FirebaseDatabase.FIRDataSnapshot
 
 class Group {
-    var key: String?
+    var key: String
     var name: String
     var dictValue: [String : Any] {
         
-        return ["group_name" : name,
-                "members" : imageHeight,
-                "created_at" : createdAgo,
-                "like_count" : likeCount,
-                "poster" : userDict]
+        return ["group_name" : name]
     }
     
-    init(name: String) {
+    init(key: String, name: String) {
         self.name = name
+        self.key = key
     }
     
-}*/
+}
--- signin/GroupService.swift
+++ signin/GroupService.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Shannon Ding. All rights reserved.
 //
 
-/*import Foundation
+import Foundation
 import FirebaseAuth.FIRUser
 import FirebaseDatabase
 
@@ -14,23 +14,28 @@ import FirebaseDatabase
 struct GroupService {
 
     var groups: [Group]
+    var groupKey: String?
     
-    private static func create(groupName: String) {
+    static func create(groupName: String) {
         // create new group in database
         
         
-        // list of group keys under user uid profile
         // list of uids inside a group key (group_members and group_admins)
         // list of other attributes under group key
         let currentUser = User.current
-        // 2
-        let group = Group(name: groupName)
+        
+        let ref = Database.database().reference().child("groups_about").childByAutoId()
+        
+        let key = ref.key
+        let group = Group(key: key, name: groupName)
         // 3
         let dict = group.dictValue
         
-        // 4
-        let postRef = Database.database().reference().child(currentUser.uid).child("groups").childByAutoId()
-        //5
-        postRef.updateChildValues(dict)
+        ref.updateChildValues(dict)
+        
+        let baseRef = Database.database().reference()
+        baseRef.child("groups_joined").child(currentUser.uid).child(key).updateChildValues(dict)
+        //baseRef.child("group_members").child(key).child( //user array)
+        
     }
-}*/
+}
