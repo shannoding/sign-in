44fc62986d8d72c6a95b0994b3f90c4701f98235
Shannon Ding <shannon_ding@yahoo.com>
2017-08-03 10:24:54 -0700

    dates work

--- signin/CreateEventViewController.swift
+++ signin/CreateEventViewController.swift
@@ -15,17 +15,33 @@ class CreateEventViewController: UIViewController {
     
     @IBOutlet weak var eventNameTextField: UITextField!
     @IBOutlet weak var eventDateTextField: UITextField!
+    var DatePickerView  : UIDatePicker = UIDatePicker()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        DatePickerView.datePickerMode = UIDatePickerMode.date
+        eventDateTextField.inputView = DatePickerView
+        DatePickerView.addTarget(self, action: #selector(CreateEventViewController.handleDatePicker), for: UIControlEvents.valueChanged)
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func handleDatePicker()
+    {
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "MM-dd-yyyy hh:mm:ss"
+        
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "MMM dd, yyyy"
+        
+//        let date: NSDate? = dateFormatterGet.date(from: "2016-02-29 12:24:26")
+//        print(dateFormatterPrint.stringFromDate(date!))
+        
+        eventDateTextField.text =  dateFormatterPrint.string(from: DatePickerView.date)
+    }
     @IBAction func createEventButtonTapped(_ sender: UIButton) {
         guard let eventName = eventNameTextField.text,
             let eventDate = eventDateTextField.text,
