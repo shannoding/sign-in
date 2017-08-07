bfcda0da9cc3650d6ca19f01479a47c909dc5baa
Shannon Ding <shannon_ding@yahoo.com>
2017-08-07 16:54:24 -0700

    seach works bless up

--- signin/GroupSearchViewController.swift
+++ signin/GroupSearchViewController.swift
@@ -14,7 +14,7 @@ class GroupSearchViewController: UIViewController, UITableViewDataSource, UITabl
     @IBOutlet weak var groupSearchTableView: UITableView!
     
     
-    let groupSearchController = UISearchController(searchResultsController: nil)
+    //let groupSearchController = UISearchController(searchResultsController: nil)
     
     var searchActive : Bool = false
     var data = [Group]()
@@ -36,8 +36,8 @@ class GroupSearchViewController: UIViewController, UITableViewDataSource, UITabl
         groupSearchBar.delegate = self
         
         
-        groupSearchController.searchResultsUpdater = self
-        groupSearchController.dimsBackgroundDuringPresentation = false
+        //groupSearchController.searchResultsUpdater = self
+        //groupSearchController.dimsBackgroundDuringPresentation = false
         definesPresentationContext = true
         //groupSearchTableView.tableHeaderView = searchController.searchBar
         
@@ -58,8 +58,9 @@ class GroupSearchViewController: UIViewController, UITableViewDataSource, UITabl
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(groupSearchController.isActive)
-        if groupSearchController.isActive && groupSearchBar.text != "" {
+        //print(groupSearchController.isActive)
+        //if groupSearchController.isActive && 
+        if groupSearchBar.text != "" {
             return filtered.count
         }
         return data.count
@@ -70,7 +71,8 @@ class GroupSearchViewController: UIViewController, UITableViewDataSource, UITabl
         
         let groupSearched: Group
         
-        if groupSearchController.isActive && groupSearchBar.text != "" {
+        //if groupSearchController.isActive && 
+        if groupSearchBar.text != "" {
             groupSearched = filtered[indexPath.row]
         }
         else {
@@ -90,20 +92,22 @@ class GroupSearchViewController: UIViewController, UITableViewDataSource, UITabl
         self.filtered = data.filter { group in
             return group.name.lowercased().contains(searchText.lowercased()) //range(of: searchText.lowercased()) != nil
         }
-        
         groupSearchTableView.reloadData()
+
     }
 
     
     // MARK: - Segues
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if groupSearchController.isActive && groupSearchBar.text != "" {
+        //if groupSearchController.isActive && 
+        if groupSearchBar.text != "" {
             let groupSearched = filtered[indexPath.row]
             UserService.joinGroup(uid: User.current.uid, groupKey: groupSearched.key) { (groupJoined) in
                 HomeViewController.groupSelected = groupJoined
                 print("Groups joined is \(groupJoined)")
                 print("The group selected from joining is \(HomeViewController.groupSelected)")
-                self.performSegue(withIdentifier: "showGroupSegue", sender: self)
+                self.view.endEditing(true)
+                self.performSegue(withIdentifier: "showGroupFromSearchSegue", sender: self)
             }
         }
         else {
@@ -113,6 +117,7 @@ class GroupSearchViewController: UIViewController, UITableViewDataSource, UITabl
                 HomeViewController.groupSelected = groupJoined
                 print("Groups joined is \(groupJoined)")
                 print("The group selected from joining is \(HomeViewController.groupSelected)")
+                self.view.endEditing(true)
                 self.performSegue(withIdentifier: "showGroupFromSearchSegue", sender: self)
             }
 
@@ -122,6 +127,11 @@ class GroupSearchViewController: UIViewController, UITableViewDataSource, UITabl
 }
 
 extension GroupSearchViewController: UISearchResultsUpdating {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterContentForSearchText(searchText: searchBar.text!)
+    }
+    
     func updateSearchResults(for searchController: UISearchController) {
         print("updating")
         filterContentForSearchText(searchText: searchController.searchBar.text!)
